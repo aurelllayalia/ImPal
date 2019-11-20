d98e28e5e5ce3937f8720e0547d45f0c1d9c6010
Aurell Layalia <47955196+aurelllayalia@users.noreply.github.com>
2019-11-20 16:58:34 +0700

    Add files via upload

---
+++ Header.php
@@ -0,0 +1,33 @@
+<header>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+  <button class="navbar-toggler" type="btn-outline-successon" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarCollapse">
+    <div class="left navbar-nav">
+      <a href="HomePage.php" class="linko">
+      <img src="Foto/Camel2.png" width="100" height="60"a></a>
+    </div>
+    <div class="left navbar-nav">
+      <a href="Product.php" class="linko">Product</a>
+    </div>
+    <div class="left navbar-nav">
+      <a href="Supplier.php" class="linko">Supplier</a>
+    </div>
+    <div class="left navbar-nav mr-auto">
+  </div>
+<style type="text/css" scoped>
+.GeneratedMarquee {
+  font-family:Helvetica, sans-serif;
+  font-size:1.2em;
+  font-style:italic;
+  line-height:2.8em;
+  color:#A1A1A1;
+}
+</style>
+    <div class="right"><a href="Login.php" class="link1">Log in</a></div>
+    <div class="right"><a href="Signup.php" class="link1">Sign up</a></div>
+  </div>
+  </nav>
+</header>
