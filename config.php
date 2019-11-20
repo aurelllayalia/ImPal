d5d3aa19dce1cc4192d468b7a70469a79d77cdf9
Aurell Layalia <47955196+aurelllayalia@users.noreply.github.com>
2019-11-20 16:55:57 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+	$server = "localhost";
+	$database = "tubesimpal";
+	$username = "root";
+	$password = "";
+
+	$conn = mysqli_connect($server, $username, $password, $database) or die("Koneksi gagal!");
+?>
