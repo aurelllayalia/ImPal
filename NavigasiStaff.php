160688006afb47c495787d4acfa5a6e36a86dd7d
aan <47202004+christianhary@users.noreply.github.com>
2019-11-20 22:59:10 +0700

    Add files via upload

---
+++ NavigasiStaff.php
@@ -0,0 +1,37 @@
+<header>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+  <button class="navbar-toggler" type="btn-outline-successon" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarCollapse">
+    <div class="left navbar-nav">
+      <a href="HomePage.php" class="linko">
+      <img src="Foto/Camel2.png" width="100" height="60"a></a>
+    </div>
+    <div class="left navbar-nav">
+    <a href="MenuStaff.php" class="linko">Data Supplier</a>
+    </div>
+    <div class="left navbar-nav">
+    <a href="MenuRequestStaff.php" class="linko">Data Request</a>
+    </div>
+    <div class="left navbar-nav">
+    <a href="MenuBarang.php" class="linko">Data Barang</a>
+    </div>
+    <div class="left navbar-nav">
+    <a href="CreateBarang.php" class="linko">Create Barang</a>
+    </div>
+  </div>
+<style type="text/css" scoped>
+.GeneratedMarquee {
+  font-family:Helvetica, sans-serif;
+  font-size:1.2em;
+  font-style:italic;
+  line-height:2.8em;
+  color:#A1A1A1;
+}
+</style>
+<div class="right"><a href="Logout.php" class="link1">Log Out</a></div>
+  </div>
+  </nav>
+</header>
