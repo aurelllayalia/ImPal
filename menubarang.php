e4337f36eaa2abf7f0be6194ac64e228bc553e2f
brigita tenggehi <47953620+brigitatenggehi@users.noreply.github.com>
2019-11-20 17:10:17 +0700

    create menubarang.php
    halaman ini menampilkan barang yang juga bisa di edit dan hapus

---
+++ menubarang.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Staff - Barang</title>
+  <link href="User/css/bootstrap-4.0.0.css" rel="stylesheet">
+    <link href="User/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link href="User/css/jquery.dataTables.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="NavigasiStaff.css">
+    <link rel="stylesheet" type="text/css" href="MenuStaff.css">
+</head>
+<body>
+  <?php
+    include "NavigasiStaff.php";
+  ?>
+  <br>
+  <br>
+  <br>
+  <br>
+  
