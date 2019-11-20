0e50b41ab048658d357ce9dbf7c95724de2ec5d8
Aurell Layalia <47955196+aurelllayalia@users.noreply.github.com>
2019-11-20 16:58:59 +0700

    Add files via upload

---
+++ HeaderLogin.php
@@ -0,0 +1,24 @@
+<header>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+  <button class="navbar-toggler" type="btn-outline-successon" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarCollapse">
+    <div class="left navbar-nav">
+      <a href="HomePage.php" class="linko">Home</a></span></a>
+    </div>
+    <div class="left navbar-nav">
+      <a href="Product.php" class="linko">Product</a></span></a>
+    </div>
+    <div class="left navbar-nav">
+      <a href="Supplier.php" class="linko">Supplier</a>
+    </div>
+    <div class="left navbar-nav mr-auto">
+    </div>
+    <div class="right">
+      <a href="Logout.php" class="link1">Log Out</a>
+    </div>
+  </div>
+  </nav>
+</header>
